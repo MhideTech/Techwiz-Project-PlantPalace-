2756bfe2615b1de69544bde1e714f20b2d0aa044
Mhizta-orlah <okefolahanolamide2006@gmail.com>
2023-08-09 13:05:23 +0100

    Navbar and Landing Page Modification

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,15 +1,17 @@
-const navBar = document.querySelector(`.nav`);
-const openNav = document.querySelector(`.nav-open-button`);
-
-class App {
-  constructor() {
-    openNav.addEventListener(`click`, this.#openNav);
+const nav = document.querySelector("nav");
+document.addEventListener("scroll", function () {
+  if (window.scrollY > 174) {
+    nav.style.backgroundColor = "rgb(0, 0, 0)";
+    nav.style.width = "100%";
+    nav.style.padding = "10px 20px"
+    nav.style.backdropFilter = "blur(10px)";
+    nav.style.height = "fit-content";
+    nav.style.borderBottom = "0px solid white";
+  } else {
+    nav.style.width = "90%";
+    nav.style.borderBottom = "1px solid white";
+    nav.style.height = "13vh";
+    nav.style.backdropFilter = "blur(0)";
+    nav.style.backgroundColor = "rgba(0,0,0,0)";
   }
-
-  #openNav() {
-    openNav.classList.toggle(`close`);
-    navBar.classList.toggle(`hidden`);
-  }
-}
-
-const app = new App();
+});
