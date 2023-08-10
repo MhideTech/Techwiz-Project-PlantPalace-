2f903f50ccb5991c4b9931c31b48b4ba1b3abb85
divineamunega <114052520+divineamunega@users.noreply.github.com>
2023-08-10 11:11:15 +0100

    Addded the contribution

---
+++ static/javascript/category.js
@@ -0,0 +1,182 @@
+const modal = document.querySelector(`.modalPlant`);
+const modalCloseBtn = document.querySelector(`.modal-close`);
+const overlay = document.querySelector(`.overlay`);
+const filterContainer = document.querySelector(".filter");
+const plantContainer = document.querySelector(".plant-cards");
+const inputSearch = document.querySelector(`.search-plants`);
+let plantDataArr;
+const openModal = function () {
+	modal.classList.remove(`hidden`);
+	overlay.classList.remove(`hidden`);
+};
+
+const closeModal = function () {
+	modal.classList.add(`hidden`);
+	overlay.classList.add(`hidden`);
+};
+
+const fetchAll = async function () {
+	try {
+		const res = await fetch("../json/plants_data.json");
+		const data = await res.json();
+		plantDataArr = data;
+
+		if (!res.ok) throw new Error("Failed to get data" + res.status);
+
+		return data;
+	} catch (err) {
+		return err.message;
+	}
+};
+
+const fetchByCategory = async function (category) {
+	try {
+		const res = await fetchAll();
+		if (category === "all") {
+			return res;
+		}
+		const data = res.filter((plant) => plant.category === category);
+		plantDataArr = data;
+		return data;
+	} catch (err) {
+		console.log(err.message);
+		return err.message;
+	}
+};
+const createPlantCard = function ({
+	image_url: img,
+	name: plantName,
+	price: plantPrice,
+	category: plantCategory,
+}) {
+	const plantCard = `				
+    <div class="plant-card">
+    <div class="img-cont">
+        <img src="${img}" alt="${plantName}" />
+    </div>
+    <div class="content">
+        <p>Name : ${plantName}</p>
+        <p>Price : $${plantPrice}</p>
+        <p>Category : ${plantCategory} Plant</p>
+        <button class="add-to-cart">Add To Cart</button>
+        <button class="more-info">More Information</button>
+    </div>
+</div>`;
+	return plantCard;
+};
+
+const handlePagination = function (page, plantData, itemsPerPage) {
+    const buttons = document.querySelectorAll(`#pagination button`);
+	const startIndex = (page - 1) * itemsPerPage;
+    buttons.forEach(btn => btn.classList.remove(`active`));
+    buttons[page - 1].classList.add(`active`);
+	const endIndex = startIndex + itemsPerPage;
+	const plantArray = plantData.slice(startIndex, endIndex);
+	renderPlantData(plantArray);
+};
+
+const renderPlantData = function (plantData) {
+	plantContainer.innerHTML = "";
+	plantData.forEach((plant) => {
+		const plantCard = createPlantCard(plant);
+		plantContainer.insertAdjacentHTML("beforeend", plantCard);
+	});
+};
+
+const handleFilter = async function (e) {
+	// Guard Clause
+	if (
+		e.target.classList.contains("sort") ||
+		!e.target.classList.contains(`filterButton`)
+	)
+		return;
+
+	const element = e.target.closest(".filterButton");
+	console.log(element.dataset.filter);
+	const data = await fetchByCategory(element.dataset.filter);
+	initPaginationCtrl(6, data);
+	handlePagination(1, data, 6);
+	// renderPlantData(data)
+};
+
+const initPaginationCtrl = function (itemsPerPage, plantData) {
+	const paginationContainer = document.getElementById("pagination");
+	paginationContainer.textContent = "";
+	const totalPages = Math.ceil(plantData.length / itemsPerPage);
+	for (let i = 1; i <= totalPages; i++) {
+		const pageButton = document.createElement("button");
+		pageButton.textContent = i;
+        pageButton.classList.add(`active`);
+		pageButton.addEventListener("click", function () {
+			handlePagination(i, plantData, itemsPerPage);
+            pageButton.classList.add(`active`);
+		});
+		paginationContainer.appendChild(pageButton);
+	}
+};
+
+const renderAll = async function () {
+	try {
+		const data = await fetchAll();
+		// console.log(data.length);
+		initPaginationCtrl(6, data);
+		handlePagination(1, data, 6);
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+const sortNRender = function (data, sort = "alphabet") {
+	if (sort === "alphabet") {
+		const dataSorted = data.sort((a, b) => a.name.localeCompare(b.name));
+		initPaginationCtrl(6, dataSorted);
+		handlePagination(1, dataSorted, 6);
+	}
+
+	if (sort === "price") {
+		const dataSorted = data.sort((a, b) => a.price - b.price);
+		initPaginationCtrl(6, dataSorted);
+		handlePagination(1, dataSorted, 6);
+	}
+};
+
+const handleSort = function (e) {
+	if (!e.target.classList.contains("sort-option")) return;
+	const element = e.target.closest(`.sort-option`);
+	sortNRender(plantDataArr, element.dataset.sort);
+};
+
+const searchNRender = function (query, data) {
+	const lowerCaseQuery = query.toLowerCase();
+	const dataSearch = data.filter((plant) => {
+		for (const key in plant) {
+			if (
+				typeof plant[key] === "string" &&
+				plant[key].toLowerCase().includes(lowerCaseQuery)
+			) {
+				return true;
+			}
+		}
+		return false;
+	});
+	initPaginationCtrl(6, dataSearch);
+	handlePagination(1, dataSearch, 6);
+};
+
+const handleSearch = function () {
+    // console.log(inputSearch.value.length);
+    if(inputSearch.value.length === 0){
+        renderAll();
+        return
+    }
+	searchNRender(inputSearch.value, plantDataArr);
+};
+
+renderAll();
+filterContainer.addEventListener("click", handleSort);
+filterContainer.addEventListener(`click`, handleFilter);
+modalCloseBtn.addEventListener("click", closeModal);
+inputSearch.addEventListener("keydown", handleSearch);
+
+// Create Active Class For Filter Links
+// Add Sort by popularity as an option
--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,17 +1,29 @@
 const nav = document.querySelector("nav");
 document.addEventListener("scroll", function () {
-  if (window.scrollY > 174) {
-    nav.style.backgroundColor = "rgb(0, 0, 0)";
-    nav.style.width = "100%";
-    nav.style.padding = "10px 20px"
-    nav.style.backdropFilter = "blur(10px)";
-    nav.style.height = "fit-content";
-    nav.style.borderBottom = "0px solid white";
-  } else {
-    nav.style.width = "90%";
-    nav.style.borderBottom = "1px solid white";
-    nav.style.height = "13vh";
-    nav.style.backdropFilter = "blur(0)";
-    nav.style.backgroundColor = "rgba(0,0,0,0)";
-  }
+	if (window.scrollY > 174) {
+		nav.style.backgroundColor = "rgb(0, 0, 0)";
+		nav.style.width = "100%";
+		nav.style.padding = "10px 20px";
+		nav.style.backdropFilter = "blur(10px)";
+		nav.style.height = "fit-content";
+		nav.style.borderBottom = "0px solid white";
+	} else {
+		nav.style.width = "90%";
+		nav.style.borderBottom = "1px solid white";
+		nav.style.height = "13vh";
+		nav.style.backdropFilter = "blur(0)";
+		nav.style.backgroundColor = "rgba(0,0,0,0)";
+	}
 });
+
+const editNav = function () {
+	const observer = IntersectionObserver((obs) => {
+
+  },{
+    
+  });
+};
+
+const openModal = function(){
+	
+}
