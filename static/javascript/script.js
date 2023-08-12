6d3079906a4b6c4b1285b3527b3cef28e2bdd24f
Mhizta-orlah <okefolahanolamide2006@gmail.com>
2023-08-12 02:19:27 +0100

    Chnaged the hover state for navigation links

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,11 +1,9 @@
 const nav = document.querySelector(".navbar");
 const navCollapse = document.querySelector(".navbar-collapse")
-console.log(nav);
 document.addEventListener("scroll", function () {
 	if (window.scrollY > 80) {
 		nav.classList.add("afterScroll");
 		nav.classList.remove("b4Scroll");
-
 	} else {
 		nav.classList.add("b4Scroll");
 		nav.classList.remove("afterScroll");
